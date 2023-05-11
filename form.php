0a443d72b026071b17b2180f88a2c09e360a06da
Kayiyan <126185640+Kayiyan@users.noreply.github.com>
2023-05-12 00:36:51 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,24 @@
+
+
+<?php
+session_start();
+// after login form
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Retrieve form data
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Perform authentication (this is a simple example, DO NOT use in production)
+    if ($username == 'admin' && $password == 'password') {
+        // Successful login, set session variables
+        $_SESSION['username'] = $username;
+        header('Location: dashboard.php');
+        exit;
+    } else {
+        // Invalid credentials, show error message
+        echo 'Invalid username or password. Please try again.';
+    }
+}
+
+
---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+// Clear session data and redirect to login page
+session_unset();
+session_destroy();
+header('Location: index.php');
+exit;
+?>

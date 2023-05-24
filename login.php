9babfc96c68174242c4417ee90e70be08099f331
Kayiyan <126185640+Kayiyan@users.noreply.github.com>
2023-05-25 00:49:57 +0700

    Update login.php

--- login.php
+++ login.php
@@ -85,7 +85,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap p-0">
 		      	<h3 class="mb-4 text-center">Have an account?</h3>
-		      	<form action="#" class="signin-form">
+		      	<form action="#" class="signin-form" method="POST" >
 		      		<div class="form-group">
 		      			<input type="text" class="form-control" placeholder="Username" required>
 		      		</div>
